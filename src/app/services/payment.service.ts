dd32e3a46de9de443283d79ceb58a2743adc393d
Enesayyildiz1 <66443194+Enesayyildiz1@users.noreply.github.com>
2021-04-05 23:05:59 +0300

    5.04.2021

--- src/app/services/payment.service.ts
+++ src/app/services/payment.service.ts
@@ -2,21 +2,22 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ListResponseModel } from '../models/listResponseModel';
-import { PaymentCard } from '../models/paymentCard';
+import { FakeCard } from '../models/fakeCard';
+import { Rental } from '../models/rental';
 import { ResponseModel } from '../models/responseModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaymentService {
-  apiUrl = 'https://localhost:44335/api/payments/';
+  apiUrl = 'https://localhost:44305/';
   constructor(private httpClient: HttpClient) {}
 
-  add(rental: PaymentCard): Observable<ResponseModel> {
-    return this.httpClient.post<ResponseModel>(this.apiUrl + 'add', rental);
+  pay(rental:Rental,amount:number){
+    let path = this.apiUrl + "rentals/paymentadd";
+    //rental.returnDate = undefined;
+    this.httpClient.post<ResponseModel>(path,{payment:{amount:amount},rental:rental})
   }
-
-
 }
 
 
